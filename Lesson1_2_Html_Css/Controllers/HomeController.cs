c8c6c3db3194ed7ebd706a9c3553eab3f1af5ca1
yarencekemci <yarencekemci@gmail.com>
2026-04-02 21:00:58 +0300

    2.04.2026

---
+++ Lesson1_2_Html_Css/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Lesson1_2_Html_Css.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lesson1_2_Html_Css.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lesson1_2_Html_Css/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson1_2_Html_Css.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
