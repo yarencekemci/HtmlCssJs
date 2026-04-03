caaf00e0a294f10a1d75d3f91697cf06d053800d
yarencekemci <yarencekemci@gmail.com>
2026-04-03 21:40:33 +0300

    Css-Tag-Class-Id_Islemleri

---
+++ Lesson2_2_css_class_id/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using Lesson2_2_css_class_id.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lesson2_2_css_class_id.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult DersCss()
+        {
+            return View(); 
+        }
+
+    }
+}
---
+++ Lesson2_2_css_class_id/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson2_2_css_class_id.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
