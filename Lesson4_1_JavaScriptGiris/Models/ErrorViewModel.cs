515bcb3b2c0e57fd6a4308bd42831c36e6429c10
yarencekemci <yarencekemci@gmail.com>
2026-04-09 20:40:16 +0300

    Javascript için değişkenler, function işlemleri yapıldı

---
+++ Lesson4_1_JavaScriptGiris/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson4_1_JavaScriptGiris.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
