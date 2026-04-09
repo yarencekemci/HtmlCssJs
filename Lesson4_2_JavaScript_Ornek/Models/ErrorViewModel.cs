b20ad8202281db0ba2a37294e0c39f50b692b167
yarencekemci <yarencekemci@gmail.com>
2026-04-09 21:00:31 +0300

    9.04.2026

---
+++ Lesson4_2_JavaScript_Ornek/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson4_2_JavaScript_Ornek.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
