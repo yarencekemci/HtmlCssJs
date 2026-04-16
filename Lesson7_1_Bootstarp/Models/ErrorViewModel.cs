20bd74e0e1f18193b9f382dc654238a604a547ab
yarencekemci <yarencekemci@gmail.com>
2026-04-16 21:24:33 +0300

    16.04.2026

---
+++ Lesson7_1_Bootstarp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lesson7_1_Bootstarp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
