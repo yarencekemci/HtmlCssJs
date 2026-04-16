5ec5202c0242fae10e951eb06b0364a28c50b1be
Yaren Çekemci <93402572+yarencekemci@users.noreply.github.com>
2026-04-16 16:09:14 +0300

    Delete Lesson6_1_Jquery directory

--- Lesson6_1_Jquery/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lesson6_1_Jquery.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
