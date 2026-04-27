2ffbadc4decc5922d2628e557209391a76e1596d
yarencekemci <yarencekemci@gmail.com>
2026-04-27 23:36:36 +0300

    Constructor

---
+++ Lesson9_2_Constructor_Method/Musteriler.cs
@@ -0,0 +1,37 @@
+﻿namespace Lesson9_2_Constructor_Method
+{
+    public class Musteriler
+    {
+        /// <summary>
+        /// parametresiz yapıcı method
+        /// </summary>
+        public Musteriler() //constructor method herhangi bir değişken(değer döndüren) ya da void (değer döndürmeyen) içermez
+        {
+            Console.WriteLine("Yapıcı method hiçbir parametresi olmayan çalıştı");
+        }
+
+        /// <summary>
+        /// tek parametreli yapıcı method
+        /// </summary>
+        /// <param name="isim"></param>
+        public Musteriler(string isim)
+        {
+            Console.WriteLine("string değişken-parametre olan yapısı method:" +isim);
+        }
+
+        /// <summary>
+        /// 2 parametreli yapıcı method
+        /// </summary>
+        /// <param name="isim"></param>
+        /// <param name="tarih"></param>
+        public Musteriler(string isim, DateTime tarih) 
+        {
+            
+        }
+
+        public int SiparisSayisi()
+        {
+            return 10;
+        }
+    }
+}
---
+++ Lesson9_2_Constructor_Method/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Lesson9_2_Constructor_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            constructor (yapıcı) method nedir?
+            her class altında oluşturulabilen bir method çeşididir.
+            1- değer döndürme değişkenleri ya da void anahtar kelimesi içermeyen ve class ile aynı adı taşıyan method olmalıdır.
+            2- overload olabilirler (aşırı yüklenmiş method)
+            3- class newlnwdigi gibi (instance - örnekleme) yapıldığı gibi kullanılır, bir method gibi bağımsız adı yazılarak kullanılamaz
+
+            */
+
+            Musteriler musterClass = new Musteriler();
+            Musteriler musterClass2 = new Musteriler("constructor method tek değişken");
+
+            Console.ReadLine();
+        }
+    }
+}
